4a4ae9b834df1364dbd354dc90ccee07544cffe5
Mrlai <1433054819@qq.com>
2019-11-12 19:19:32 +0800

    update

--- js/y_self.js
+++ js/y_self.js
@@ -1,4 +1,4 @@
-function Insert() {
+function Insert() { //动态添加subject
     $('.y_more').append('     <a href="javascript:;">' + localStorage.getItem('more') + '</a>')
     $('.y_more .more').css({
         " font-size": "18px",
@@ -10,15 +10,15 @@ Insert();
 
 (function() {
     var arr = {
-        "注册产品": "y_login.html",
-        "真伪查询": "y_taf.html",
-        "自助服务": "y_phone.html",
-        "支付查询": "y_pay.html",
-        "寄送快修": "y_send.html",
-        "预约维修": "y_reserve.html"
-    }
+            "注册产品": "y_login.html",
+            "真伪查询": "y_taf.html",
+            "自助服务": "y_phone.html",
+            "支付查询": "y_pay.html",
+            "寄送快修": "y_send.html",
+            "预约维修": "y_reserve.html"
+        } //静态添加
 
-    function strCode() {
+    function strCode() { //验证码随机数
         var str = ''
         for (var i = 0; i < 4; i++) {
             str += randomInt(0, 10)
@@ -28,36 +28,33 @@ Insert();
     }
 
 
-    function Res(reg, val) {
+    function Res(reg, val) { //输入框 正则判断
         if (!val) {
             return
             alert('输入不能为空')
         }
 
-        var res = reg.test(val)
+        var res = reg.test(val) //接收判断结果
         if (!res) {
             $('.login_block .content').html('输入手机号码错误')
         }
         return res
     };
 
+
     $('.login_block .but1').click(function() {
-
+        //电话号码正则
         var reg = /^(1)\d{10}$/
-
-        // console.log($(' .wrap_number .phoneNumber').val());
-
-        var result = Res(reg, $(' .wrap_number .phoneNumber').val())
-        if (!result) {
-
+            // console.log($(' .wrap_number .phoneNumber').val());
+        var result = Res(reg, $(' .wrap_number .phoneNumber').val()) //获取输入框的值（电话号码）
+        if (!result) { //不符合是结束程序
             return
         }
-
         if (result) {
             // if(localStorage.getItem('user')){
             // }
-            localStorage.setItem('username', $(' .wrap_number .phoneNumber').val())
-            $('  .auto_code').css({
+            localStorage.setItem('username', $(' .wrap_number .phoneNumber').val()) //把电话号码保存到本地 存储
+            $('  .auto_code').css({ //验证码框block
                 'display': 'block'
             })
             $('.login_block .but1').css('display', 'none')
@@ -66,7 +63,7 @@ Insert();
 
             $('.login_block .but2').click(function() {
                 // console.log($('.auto_code .code').val());
-
+                // 判断验证码是否正确 并判断是否返回去改动了用户名
                 if ($('.auto_code .code').val() == localStorage.getItem('res') && Res(reg, $(' .wrap_number .phoneNumber').val())) {
 
                     // if (localStorage.getItem('user')) { //验证用户名
@@ -83,7 +80,7 @@ Insert();
                     $('.login_block .but2').css('display', 'none')
 
                 } else {
-                    localStorage.setItem('username', $(' .wrap_number .phoneNumber').val())
+                    localStorage.setItem('username', $(' .wrap_number .phoneNumber').val()) //如果改动用户名则重新把用户名保存本地返回去判断
                     strCode()
                     $('.login_block .content').html('验证码或手机号码输入错误')
                     $('.login_block .but1').css('display', 'none')
