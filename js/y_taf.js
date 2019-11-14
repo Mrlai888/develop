81a3ffc805fdb454f7fc45bb6fa9ac83ca42f46e
Mrlai <1433054819@qq.com>
2019-11-14 14:18:13 +0800

    up

--- js/y_taf.js
+++ js/y_taf.js
@@ -12,31 +12,42 @@ Insert();
 
 (function() {
     $('.y_taf_page .but').click(function() {
+
         var oSn = $('.y_taf_page .sn input').val()
-            // console.log(oSn)
         var oMei = $(' .y_taf_page .mei input').val()
-            // console.log(oMei);
         $.ajax({
             type: "get",
             url: "./y_taf.json",
             data: "sn=" + oSn + "&IMEI=" + oMei + "",
             dataType: "json",
             success: function(response) {
-                // console.log(response[0]);
+                console.log(response[0]);
                 var res = response[0]
-                for (var val in res) {
-                    // console.log(val, res[val]);
-                    if (oSn === val) {
-                        if (oMei === res[val]) {
+
+                for (var item in res) {
+                    console.log(typeof $('.y_taf_page .sn input').val());
+                    if ($('.y_taf_page .sn input').val() && $(' .y_taf_page .mei input').val()) {
+                        if ($('.y_taf_page .sn input').val() == item && $(' .y_taf_page .mei input').val() == res[item]) {
+                            console.log(666);
                             $('.y_taf_page .inner').html('卧槽这是真的')
-                            location.href = './servers.html'
+                                // $('.y_taf_page .inner').html('卧槽这是真的')
+                            setTimeout(() => {
+                                location.href = './servers.html'
+                            }, 1000);
+                            break
+
                         } else {
-                            console.log(777)
+
                             $('.y_taf_page .inner').html('所查 SN 无记录，请确认输入是否正确')
+
                         }
 
+                    } else {
+                        $('.y_taf_page .inner').html('账号/IMEI不能为空')
                     }
+
                 }
+
             },
 
         });
